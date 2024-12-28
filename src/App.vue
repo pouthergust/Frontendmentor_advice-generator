7dac16bc0108dab25ec2cdc2e3e4f859cb2db896
Gabriel Henrique <gabriel.henrique@agenciam3.com>
2024-12-28 17:01:49 -0300

    :tada: Challenge finished

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import AdviceViewer from './components/AdviceViewer.vue'
+</script>
+
+<template>
+  <main>
+    <AdviceViewer></AdviceViewer>
+  </main>
+
+  <div class="attribution">
+    Challenge by
+    <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. Coded
+    by <a href="#">Your Name Here</a>.
+  </div>
+</template>
+
+<style scoped>
+.attribution {
+  margin-top: 64px;
+  color: #cee3e9;
+}
+</style>
---
+++ src/components/AdviceViewer.vue
@@ -0,0 +1,80 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import PatternDividerDesktop from './icons/PatternDividerDesktop.vue'
+import DiceIcon from './icons/DiceIcon.vue'
+
+defineProps<{
+  msg: string
+}>()
+
+const advice = ref<string>('')
+const id = ref<string>('')
+
+const fetchAdvice = async () => {
+  return await fetch('https://api.adviceslip.com/advice')
+    .then((res) => res.json())
+    .then(({ slip }) => {
+      advice.value = slip.advice
+      id.value = slip.id
+    })
+    .error((err) => console.log('Error detected!', err))
+}
+
+onMounted(() => {
+  fetchAdvice()
+})
+</script>
+
+<template>
+  <div class="advice__container">
+    <span class="advice__number">ADVICE #{{ id }}</span>
+    <p class="advice__description">
+      {{ advice }}
+    </p>
+    <PatternDividerDesktop></PatternDividerDesktop>
+    <button class="advice__dice-container" @click="fetchAdvice">
+      <DiceIcon></DiceIcon>
+    </button>
+  </div>
+</template>
+
+<style scoped>
+.advice__container {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  padding: 16px 24px 0;
+  max-width: 700px;
+}
+
+.advice__number {
+  color: #59fcad;
+  font-weight: 700;
+  letter-spacing: 5px;
+}
+
+.advice__description {
+  color: #cee3e9;
+  font-size: 34px;
+  font-weight: 700;
+  text-align: center;
+}
+
+.advice__dice-container {
+  aspect-ratio: 1;
+  background-color: #59fcad;
+  border: none;
+  clip-path: circle();
+  margin-bottom: -26px;
+  padding: 16px;
+  transition: scale ease-in-out 0.1s;
+}
+
+.advice__dice-container:hover {
+  cursor: pointer;
+}
+
+.advice__dice-container:active {
+  scale: 0.95;
+}
+</style>
